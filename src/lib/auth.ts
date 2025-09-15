084446c2629384f05d2bf4c739bf82c2d8a41bed
Thanyapisit Limpakanon <152508112+ThanyapisitLim@users.noreply.github.com>
2025-09-15 23:50:53 +0700

    everything ok

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,7 +14,6 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/login",
-    error: "/login/error",
   },
   session: {
     strategy: "jwt",
